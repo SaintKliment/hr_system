72cf9f11d1e6e7e9e5de25cf6697de27719d0e38
hakesho <113922134+hakeshooo@users.noreply.github.com>
2024-12-05 18:39:03 +0300

    Update module.py

--- models/module.py
+++ models/module.py
@@ -2,17 +2,16 @@ from db import db
 
 class Module(db.Model):
     __tablename__ = 'modules'
-    __table_args__ = {'extend_existing': True}
-    
+    __table_args__ = {'extend_existing': True} 
+
     id = db.Column(db.Integer, primary_key=True)
     module_name = db.Column(db.String(255), nullable=False)
-    positions = db.Column(db.JSON, nullable=False)  
+    positions = db.Column(db.JSON, nullable=False) 
     activities = db.Column(db.JSON, nullable=False)  
     data_source = db.Column(db.String(255), nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
+    duration = db.Column(db.Integer, nullable=False) 
     responsible = db.Column(db.String(255), nullable=False)
-    materials = db.Column(db.JSON, nullable=True)  
+    materials = db.Column(db.JSON, nullable=True) 
 
-
-def __repr__(self):
+    def __repr__(self):
         return f"<Module {self.module_name}>"
